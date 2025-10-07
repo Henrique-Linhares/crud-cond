65ade44e87a2344ce2a669d76cb2d2207c9deea0
Henrique-Linhares <hlinhares453@gmail.com>
2025-10-06 21:03:52 -0300

    Implementação dos componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { useState } from 'react';
-import { Login } from './components/Login';
+import { Login } from './components/Login/Login';
+import {Welcome} from "./components/Welcome/Welcome.js";
+import {Menu} from "./components/Menu/Menu.js";
+import {ContactList} from "./components/ContactList/ContactList.js";
+import {ContactForm} from "./components/ContactForm/ContactForm.js";
 
 import './App.css';
 
 function App() {
 
   const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+  const [ activeScreen, setActiveScreen] = useState("Welcome");
+  const [ contacts, setContacts] = useState(
+    {id:1, name: "Henrique Linhares", phone: "143234325"},
+    {id:2, name: "Valquiria Linhares", phone:"21242352"}
+  );
+
+  const [contactToEdit, setContactToEdit] = useState(null);
 
   function handleLogin( userName, password ) {
     if(userName === "admin" && password === "123") {
@@ -14,6 +25,12 @@ function App() {
       alert("Usuário ou senha inválida!!!")
     }
   };
+
+  const handleSaveContact = (contact) => {}
+  const handleDeleteContact = (id) => {}
+  const startEdit = (contact) => {}
+  const showCreateForm = () => {}
+  const handleNavigate = (screen) => {}
 
   if (!isLoggedIn) {
     return <Login onLogin={handleLogin}/>
